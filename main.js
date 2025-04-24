6ddea8f097fdf5105247723fb1d824e1e86e525a
Felipe Coll <leonfelipecoll@gmail.com>
2025-04-23 21:31:22 -0300

    Maths con Javascript - operaciones basicas

---
+++ main.js
@@ -0,0 +1,13 @@
+const ladoCuadrado = 5
+const perimetro = ladoCuadrado * 4
+const areaCuadrado = ladoCuadrado * ladoCuadrado
+
+console.log(ladoCuadrado, perimetro, areaCuadrado)
+
+const ladoTriangulo1 = 6
+const ladoTriangulo2 = 6
+const ladoTrianguoBase = 4
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguoBase
+const areaTriangulo = (ladoTriangulo1 * ladoTrianguoBase) / 2
+console.log(ladoTriangulo1, ladoTriangulo2, ladoTrianguoBase, perimetroTriangulo, areaTriangulo)
